844ed665827e613dc852303dbfc37a43e6a12d3b
Maciej Korsan <maciejkorsan@gmail.com>
2017-09-15 20:39:19 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-gulp-preview-cshtml
+# gulp-preview-cshtml
+
+> Allows frontend developers using MacOS viewing cshtml files. Helpful with Umbraco frontend development. (https://github.com/maciejkorsan/gulp-preview-cshtml)
+
+
+## Install
+
+```
+$ npm install --D gulp-preview-cshtml
+```
+
+
+## Usage
+
+```js
+const gulp = require('gulp');
+const previewcshtml = require('gulp-preview-cshtml');
+
+gulp.task('default', () =>
+	gulp.src('src/app.css')
+		.pipe(previewcshtml({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist'))
+);
+```
+
+
+## License
+
+MIT © [Maciej Korsan]
---
+++ index.js
@@ -0,0 +1,47 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = require('gulp-util').PluginError;
+var fs = require("fs");
+// consts
+var PLUGIN_NAME = 'gulp-preview-cshtml';
+
+module.exports = function(opts) {
+    return through.obj(function(file, encoding, callback) {
+        if (file.isNull()) {
+            // nothing to do
+            return callback(null, file);
+        }
+
+        if (file.isStream()) {
+            // file.contents is a Stream - https://nodejs.org/api/stream.html
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+
+            // or, if you can handle Streams:
+            //file.contents = file.contents.pipe(...
+            //return callback(null, file);
+        } else if (file.isBuffer()) {
+            // file.contents is a Buffer - https://nodejs.org/api/buffer.html
+            let content = file.contents.toString();
+
+            const lines = new RegExp('@[a-zA-Z].+',"gm");
+            const comments = new RegExp('@\\*[^\\*]+\\*@',"gm");
+            const blocks = new RegExp('@{[^}]+}',"gm");
+            let layout = fs.readFileSync(opts);
+            layout = layout.toString();
+           
+            layout = layout.replace('@RenderBody()',content);
+
+            layout = layout.replace(comments,'');
+            layout = layout.replace(blocks,'');
+            layout = layout.replace(lines,'');
+
+
+            console.log(layout);
+
+            file.contents =  Buffer.from( layout, 'utf8' );            
+
+
+            return callback (null, file);
+        }
+    });
+};
