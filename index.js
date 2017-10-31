cdd9ced6ce746f967afca1ad4079b7fe4dd122f9
Maciej Korsan <maciejkorsan@gmail.com>
2017-10-31 16:04:13 +0100

    feat: Partial support

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 // consts
 var PLUGIN_NAME = 'gulp-preview-cshtml';
 
-module.exports = function(opts) {
+module.exports = function(opts, projectSource) {
     return through.obj(function(file, encoding, callback) {
         if (file.isNull()) {
             // nothing to do
@@ -23,13 +23,32 @@ module.exports = function(opts) {
             // file.contents is a Buffer - https://nodejs.org/api/buffer.html
             let content = file.contents.toString();
 
+
+            const includes = new RegExp('@Html.Partial\\(\\"(.*)\\"\\)',"gm");
             const lines = new RegExp('[^a-z]@[a-zA-Z].+',"gm");
             const comments = new RegExp('@\\*[^\\*]+\\*@',"gm");
             const blocks = new RegExp('@{[^}]+}',"gm");
             let layout = fs.readFileSync(opts);
             layout = layout.toString();
-           
+
+
+
+
             layout = layout.replace('@RenderBody()',content);
+
+
+            let match = includes.exec(layout);
+            while (match != null) {
+                let filename = match[1].replace('.cshtml', '.ref.cshtml');
+                
+                let partial = fs.readFileSync(projectSource+''+filename);
+                
+                layout = layout.replace(match[0],partial.toString());
+
+
+                match = includes.exec(layout);
+            }          
+
 
             layout = layout.replace(comments,'');
             layout = layout.replace(blocks,'');
